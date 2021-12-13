fefc177e4bfa933e690d27f4d3520ef6a8177874
Alexis Gueux <alexis.gueux@gmail.com>
2021-12-13 17:01:42 +0100

    test

--- Alexis.py
+++ Alexis.py
@@ -1,5 +1,7 @@
 from tkinter import Tk,Button,Canvas,Label
 
+from Heather import Canevas
+
 class cvaisseau():
     def __init__(self,pX_vaisseau,pY_vaisseau):
         self.__vie = 10
@@ -19,6 +21,35 @@ class cvaisseau():
             canvas.coords(vaisseau,self.__posX,self.__posY,self.__posX+50,self.__posY+50)
 
 
+class calien():
+    def __init__(self,pX_alien,pY_alien,pVitesse):
+        self.__vie = 10
+        self.__posX = pX_alien
+        self.__posY = pY_alien
+        self.__vitesse = pVitesse
+
+    def fMvmt_alien(self):
+
+        alien = None
+    
+        self.__posX += self.__vitesse
+        
+        if self.__posX >= 130:
+            self.__vitesse = -self.__vitesse
+            self.__posY +=3
+        elif self.__posX<5:
+            self.__vitesse = -self.__vitesse
+            self.__posY +=3
+        canvas.delete(alien)
+        alien=canvas.create_rectangle(self.__posX, self.__posY, self.__posX+c, self.__posY+c, fill='blue violet', outline='')
+        fen.after(50, calien.fMvmt_alien, alien)
+        
+
+        
+
+
+    
+        
 
 
 fen = Tk()
@@ -37,21 +68,21 @@ X_vaisseau = 325
 Y_vaisseau = 525
 
 X_alien = 325
-Y_alien = 5
+Y_alien = 25
+vitesse_alien = 10
 
 vaisseau = canvas.create_rectangle(325,525,325+50,525+50,fill='white')
  
 Bouton_Quitter=Button(fen, text ='Quitter', command = fen.destroy, width=15)
 Bouton_Quitter.place(x=725,y=300)
 
-Bouton_Nvlle_Partie=Button(fen, text ='Nouvelle partie', command = fen.destroy, width=15)
+Bouton_Nvlle_Partie=Button(fen, text ='Nouvelle partie', command = canvas.update(), width=15)
 Bouton_Nvlle_Partie.place(x=725,y=500)
-
-alien = canvas.create_rectangle(X_alien,Y_alien,X_alien+50,Y_alien+50,fill='green')
 
 vaisseau_init = cvaisseau(X_vaisseau, Y_vaisseau)
 
 canvas.bind('<Key>', vaisseau_init.fMouvement_vaisseau) 
+
 
 
 fen.mainloop()
